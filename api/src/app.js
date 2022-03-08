94f5e29f65c26be6994133368a34b1944000b3d7
ch <mkmsacc@outlook.com>
2022-03-08 21:06:29 +0700

    Move app.js

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,6 @@ const express = require("express");
 const app = express();
 
 app.post("/api/1.0/users", (req, res) => {
-  return res.send();
+  return res.send({ message: "User created" });
 });
 module.exports = app;
