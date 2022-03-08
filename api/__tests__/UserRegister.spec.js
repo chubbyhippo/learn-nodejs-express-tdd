4791da937aaa2ddc10241aed0329fde618f603e0
ch <mkmsacc@outlook.com>
2022-03-08 21:07:10 +0700

    Add describe

--- api/__tests__/UserRegister.spec.js
+++ api/__tests__/UserRegister.spec.js
@@ -1,16 +1,32 @@
 const request = require("supertest");
-const app = require("../app");
+const app = require("../src/app");
 
-it("should return 200 OK when signup request is valid", (done) => {
-  request(app)
-    .post("/api/1.0/users")
-    .send({
-      username: "user1",
-      email: "user1@email.com",
-      password: "P4ssw0rd",
-    })
-    .then((response) => {
-      expect(response.status).toBe(200);
-      done();
-    });
+describe("User Registration", () => {
+  it("should return 200 OK when signup request is valid", (done) => {
+    request(app)
+      .post("/api/1.0/users")
+      .send({
+        username: "user1",
+        email: "user1@email.com",
+        password: "P4ssw0rd",
+      })
+      .then((response) => {
+        expect(response.status).toBe(200);
+        done();
+      });
+  });
+
+  it("should return success message when signup request is valid", (done) => {
+    request(app)
+      .post("/api/1.0/users")
+      .send({
+        username: "user1",
+        email: "user1@email.com",
+        password: "P4ssw0rd",
+      })
+      .then((response) => {
+        expect(response.body.message).toBe("User created");
+        done();
+      });
+  });
 });
