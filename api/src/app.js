73e86fb7454ef78b5ff3ee9af3db3cca90c099cb
ch <mkmsacc@outlook.com>
2022-05-29 17:33:44 +0700

    Add _

--- api/src/app.js
+++ api/src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.post("/api/1.0/users", (req, res) => {
+app.post("/api/1.0/users", (_req, res) => {
   return res.send({ message: "User created" });
 });
 module.exports = app;
