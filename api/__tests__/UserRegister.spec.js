c0087eebd399ecbd139903f6572718a6bc1338a0
ch <mkmsacc@outlook.com>
2022-05-31 08:04:04 +0700

    Add .default

--- api/__tests__/UserRegister.spec.js
+++ api/__tests__/UserRegister.spec.js
@@ -1,5 +1,11 @@
 const request = require("supertest");
 const app = require("../src/app");
+const User = require("../src/user/User");
+const sequelize = require("../src/config/database").default;
+
+beforeAll(() => {
+  return sequelize.sync();
+});
 
 describe("User Registration", () => {
   it("should return 200 OK when signup request is valid", (done) => {
@@ -27,6 +33,23 @@ describe("User Registration", () => {
       .then((response) => {
         expect(response.body.message).toBe("User created");
         done();
+      });
+  });
+
+  it("should save the user to database", (done) => {
+    request(app)
+      .post("/api/1.0/users")
+      .send({
+        username: "user1",
+        email: "user1@email.com",
+        password: "P4ssw0rd",
+      })
+      .then(() => {
+        // query the user table
+        User.findAll().then((value) => {
+          expect(value.length).toBe(1);
+          done();
+        });
       });
   });
 });
