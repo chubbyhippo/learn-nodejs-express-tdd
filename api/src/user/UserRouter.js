19d8edb9ff645d730375fe9e9504954d7d207876
ch <mkmsacc@outlook.com>
2022-06-05 21:27:08 +0700

    Refactor user service

--- api/src/user/UserRouter.js
+++ api/src/user/UserRouter.js
@@ -1,12 +1,9 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
-const User = require("./User");
+const UserService = require("./UserService");
 const router = express.Router();
 
 router.post("/api/1.0/users", async (req, res) => {
-  const hashed = await bcrypt.hash(req.body.password, 10);
-  const user = { ...req.body, password: hashed };
-  await User.create(user);
+  await UserService.save(req.body);
   return res.send({ message: "User created" });
 });
 
