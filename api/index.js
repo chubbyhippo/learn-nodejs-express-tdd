9fc7690bf18524448c37e1550f77883067827fa6
ch <mkmsacc@outlook.com>
2022-06-07 07:30:05 +0700

    Move print env to app.js

--- api/index.js
+++ api/index.js
@@ -2,5 +2,4 @@ const app = require("./src/app");
 const sequelize = require("./src/config/database").default;
 
 sequelize.sync();
-console.log("env: " + process.env.NODE_ENV);
 app.listen(8080, () => console.log("app is running"));
