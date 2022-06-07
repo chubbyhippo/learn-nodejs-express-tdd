24d372aaf4c23a010013060d13f6729773224b9f
ch <mkmsacc@outlook.com>
2022-06-07 07:28:20 +0700

    Try printing out the env

--- api/index.js
+++ api/index.js
@@ -2,4 +2,5 @@ const app = require("./src/app");
 const sequelize = require("./src/config/database").default;
 
 sequelize.sync();
+console.log("env: " + process.env.NODE_ENV);
 app.listen(8080, () => console.log("app is running"));
