269d51ff9e8ccddaa87fa641f8d6623ba1d3cacf
ch <mkmsacc@outlook.com>
2022-06-08 17:06:04 +0700

    Refactor code using middleware

--- api/src/user/UserRouter.js
+++ api/src/user/UserRouter.js
@@ -2,17 +2,23 @@ const express = require("express");
 const UserService = require("./UserService");
 const router = express.Router();
 
-router.post("/api/1.0/users", async (req, res) => {
-  const user = req.body;
-  if (user.username === null) {
-    return res.status(400).send({
-      validationErrors: {
-        username: "Username cannot be null.",
-      },
-    });
+router.post(
+  "/api/1.0/users",
+  (req, res, next) => {
+    const user = req.body;
+    if (user.username === null) {
+      return res.status(400).send({
+        validationErrors: {
+          username: "Username cannot be null.",
+        },
+      });
+    }
+    next();
+  },
+  async (req, res) => {
+    await UserService.save(req.body);
+    return res.send({ message: "User created" });
   }
-  await UserService.save(req.body);
-  return res.send({ message: "User created" });
-});
+);
 
 module.exports = router;
