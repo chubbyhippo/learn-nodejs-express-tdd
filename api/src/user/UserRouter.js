960bfd9cc8061420e625bedb713c5b9ed7764f8d
ch <mkmsacc@outlook.com>
2022-06-08 17:11:13 +0700

    Implement should return email cannot be null when validation error occurs

--- api/src/user/UserRouter.js
+++ api/src/user/UserRouter.js
@@ -14,9 +14,26 @@ const validateUsername = (req, res, next) => {
   next();
 };
 
-router.post("/api/1.0/users", validateUsername, async (req, res) => {
-  await UserService.save(req.body);
-  return res.send({ message: "User created" });
-});
+const validateEmail = (req, res, next) => {
+  const user = req.body;
+  if (user.email === null) {
+    return res.status(400).send({
+      validationErrors: {
+        email: "Email cannot be null.",
+      },
+    });
+  }
+  next();
+};
+
+router.post(
+  "/api/1.0/users",
+  validateUsername,
+  validateEmail,
+  async (req, res) => {
+    await UserService.save(req.body);
+    return res.send({ message: "User created" });
+  }
+);
 
 module.exports = router;
