5d4aeffb0ec6855397a7ef2869e6508104f09e16
ch <mkmsacc@outlook.com>
2022-06-08 07:35:34 +0700

    Refactor test

--- api/__tests__/UserRegister.spec.js
+++ api/__tests__/UserRegister.spec.js
@@ -11,33 +11,42 @@ beforeEach(() => {
   return User.destroy({ truncate: true });
 });
 
+const validUser = {
+  username: "user1",
+  email: "user1@email.com",
+  password: "P4ssw0rd",
+};
+
+const invalidUser = {
+  username: null,
+  email: "user1@email.com",
+  password: "P4ssw0rd",
+};
+
+const postUser = (user) => {
+  return request(app).post("/api/1.0/users").send(user);
+};
+
 describe("User Registration", () => {
-  const postValidUser = () => {
-    return request(app).post("/api/1.0/users").send({
-      username: "user1",
-      email: "user1@email.com",
-      password: "P4ssw0rd",
-    });
-  };
   it("should return 200 OK when signup request is valid", async () => {
-    const response = await postValidUser();
+    const response = await postUser(validUser);
     expect(response.status).toBe(200);
   });
 
   it("should return success message when signup request is valid", async () => {
-    const response = await postValidUser();
+    const response = await postUser(validUser);
     expect(response.body.message).toBe("User created");
   });
 
   it("should save the user to database", async () => {
-    await postValidUser();
+    await postUser(validUser);
     // query the user table
     const userList = await User.findAll();
     expect(userList.length).toBe(1);
   });
 
   it("should save the username and email to database", async () => {
-    await postValidUser();
+    await postUser(validUser);
     // query the user table
     const userList = await User.findAll();
     const savedUser = userList[0];
@@ -46,7 +55,7 @@ describe("User Registration", () => {
   });
 
   it("should hashes the password in database", async () => {
-    await postValidUser();
+    await postUser(validUser);
     // query the user table
     const userList = await User.findAll();
     const savedUser = userList[0];
@@ -54,11 +63,7 @@ describe("User Registration", () => {
   });
 
   it("should return 400 when username is null", async () => {
-    const response = await request(app).post("/api/1.0/users").send({
-      username: null,
-      email: "user12@mail.com",
-      password: "P4ssw0rd",
-    });
+    const response = await postUser(invalidUser);
     expect(response.status).toBe(400);
   });
 });
