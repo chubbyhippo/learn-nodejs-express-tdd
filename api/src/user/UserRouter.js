2fcecb0deeedd4e2ffa5cf9fd4fd804873e9ad88
ch <mkmsacc@outlook.com>
2022-06-10 07:52:38 +0700

    Replace current validator with the express approach

--- api/src/user/UserRouter.js
+++ api/src/user/UserRouter.js
@@ -1,40 +1,20 @@
 const express = require("express");
 const UserService = require("./UserService");
 const router = express.Router();
-
-const validateUsername = (req, _res, next) => {
-  const user = req.body;
-  if (user.username === null) {
-    req.validationErrors = {
-      username: "Username cannot be null.",
-    };
-  }
-  next();
-};
-
-const validateEmail = (req, _res, next) => {
-  const user = req.body;
-  if (user.email === null) {
-    req.validationErrors = {
-      ...req.validationErrors,
-      email: "Email cannot be null.",
-    };
-  }
-  next();
-};
+const { check, validationResult } = require("express-validator");
 
 router.post(
   "/api/1.0/users",
-  validateUsername,
-  validateEmail,
+  check("username").notEmpty().withMessage("Username cannot be null."),
+  check("email").notEmpty().withMessage("Email cannot be null."),
   async (req, res) => {
-    if (req.validationErrors) {
-      const response = {
-        validationErrors: {
-          ...req.validationErrors,
-        },
-      };
-      return res.status(400).send(response);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const validationErrors = {};
+      errors
+        .array()
+        .forEach((error) => (validationErrors[error.param] = error.msg));
+      return res.status(400).send({ validationErrors: validationErrors });
     }
     await UserService.save(req.body);
     return res.send({ message: "User created" });
