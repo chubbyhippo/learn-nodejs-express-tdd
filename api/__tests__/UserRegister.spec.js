73077b8d7c220fcda1811c4396f1b8fdddcd1227
ch <mkmsacc@outlook.com>
2022-06-28 08:12:42 +0700

    Add more email validation

--- api/__tests__/UserRegister.spec.js
+++ api/__tests__/UserRegister.spec.js
@@ -113,6 +113,7 @@ describe("User Registration", () => {
     ${"username"} | ${"a".repeat(33)} | ${"Must have min 4 and max 32 characters."}
     ${"email"}    | ${null}           | ${"Email cannot be null."}
     ${"email"}    | ${"mail.com"}     | ${"Email is not valid."}
+    ${"email"}    | ${"user@mail"}    | ${"Email is not valid."}
     ${"password"} | ${null}           | ${"Password cannot be null."}
   `(
     "should return $expectedMessage when $field is $value",
